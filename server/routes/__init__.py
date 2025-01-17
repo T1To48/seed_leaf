3ba9ef937432819362ed84639c6608002b196fe0
T1to48 <tofek.98@gmail.com>
2025-01-17 11:41:59 +0000

    cleaning

--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -7,9 +7,6 @@ api_bp.register_blueprint(auth_bp,url_prefix="/auth")
 api_bp.register_blueprint(user_bp,url_prefix="/user")
 user_bp.register_blueprint(user_account_bp,url_prefix = "/account")
 user_bp.register_blueprint(orders_bp,url_prefix="/orders")
+
 api_bp.register_blueprint(products_bp,url_prefix="/products")
 
-
-
-
-# auth = Blueprint('auth',__name__,url_prefix="/products")
