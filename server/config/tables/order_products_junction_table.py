b21e6cc01fca32e9f23265ca215c47392a8a2b19
T1to48 <tofek.98@gmail.com>
2025-01-19 21:04:14 +0000

    modified tables column name , for better accuracy

--- server/config/tables/order_products_junction_table.py
+++ server/config/tables/order_products_junction_table.py
@@ -3,7 +3,7 @@ CREATE TABLE order_products_junction(
 order_id CHAR(36),
 product_id CHAR(36),
 quantity SMALLINT NOT NULL,
-purchase_price FLOAT NOT NULL,
+purchase_price DECIMAL(10, 2) NOT NULL,
 PRIMARY KEY (order_id,product_id),
 FOREIGN KEY (order_id) REFERENCES orders(order_id),
 FOREIGN KEY (product_id) REFERENCES products(product_id)
--- server/config/tables/orders_table.py
+++ server/config/tables/orders_table.py
@@ -2,7 +2,7 @@ orders_table = """
 CREATE TABLE orders(
 order_id CHAR(36),
 user_id CHAR(36),
-order_price FLOAT NOT NULL,
+order_price DECIMAL(10, 2) NOT NULL,
 date_time DATETIME  NOT NULL DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY (order_id),
 FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE SET NULL
