e0a431a78872fcfaec9a4f4edaf946caac060739
T1to48 <tofek.98@gmail.com>
2025-01-20 22:36:08 +0000

    debugged and corrected get_cart_product_quantity. enhanced update_cart_product_quantity to be  dynamic. added function delete_cart_product

--- server/services/user/cart/cart_helpers.py
+++ server/services/user/cart/cart_helpers.py
@@ -2,15 +2,36 @@ from utils import get_from_database,update_row_database
 
 def get_cart_product_quantity(user_id,product_id):
     exec_statement = """SELECT quantity FROM cart_items WHERE user_id = %s AND product_id = %s"""
-    return get_from_database(exec_statement,[user_id,product_id],True).get("quantity")
+    cart_product_quantity = get_from_database(exec_statement,[user_id,product_id],True)
 
-def update_cart_product_quantity(user_id,product_id):
+    if not cart_product_quantity:
+        return False
+
+    return cart_product_quantity.get("quantity")
+
+def update_cart_product_quantity(user_id,product_id,delta):
     quantity = get_cart_product_quantity(user_id,product_id)
-    quantity +=1
+    if not quantity:
+        return False
+    if quantity == 1 and delta == -1:
+        return delete_cart_product(user_id,product_id)
+
+    quantity +=delta
     exec_statement = """UPDATE cart_items SET quantity = %s WHERE user_id = %s AND product_id = %s"""
     data_list = [quantity,user_id,product_id]
     db_cart_update = update_row_database(exec_statement,data_list)
     
-    if db_cart_update and db_cart_update != "DB_ERROR" and db_cart_update!= "IntegrityError":
+    if db_cart_update and db_cart_update not in {"DB_ERROR","IntegrityError"}:
         return True
     return False
+
+
+def delete_cart_product(user_id,product_id):
+    exec_statement = """DELETE FROM cart_items WHERE user_id = %s AND product_id = %s"""
+    db_cart_product_delete = update_row_database(exec_statement,[user_id,product_id])
+
+    if db_cart_product_delete and db_cart_product_delete not in {"DB_ERROR","IntegrityError"}:
+        return True
+    
+    return False
+
