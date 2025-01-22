f5f34272559e1d1664c6a1e3c7e72cb9947b0dc6
T1to48 <tofek.98@gmail.com>
2025-01-22 00:25:52 +0000

    added helper function that gets the order details and a func to get order products details

--- server/services/user/orders/orders_helpers.py
+++ server/services/user/orders/orders_helpers.py
@@ -1,4 +1,4 @@
-from utils import update_row_database,update_many_rows_database
+from utils import update_row_database,update_many_rows_database,get_from_database
 
 def add_order_database(user_id,order_id,order_price):
     exec_statement = """INSERT INTO orders (order_id,user_id, order_price) VALUES (%s, %s, %s)"""
@@ -30,3 +30,35 @@ def convert_to_lists(dicts_list,order_id):
             dict["product_price"]
         ]
     return dicts_list
+
+def get_order_products_database(order_id):
+    exec_statement = """SELECT p.product_id, p.product_name, p.product_img,
+                        p.product_category, opj.purchase_price,
+                        opj.quantity, opj.total_purchase_price
+                        from order_products_junction opj
+                        JOIN products p USING (product_id)
+                        WHERE order_id = %s ;"""
+    
+    return get_from_database(exec_statement,[order_id])
+
+
+def get_order_details_database(user_id,order_id):
+    exec_statement = """ SELECT * FROM orders WHERE user_id = %s AND order_id = %s """
+    order_details_database = get_from_database(exec_statement,[user_id,order_id],True)
+    if not order_details_database or order_details_database == "DB_ERROR":
+        return False
+    
+    order_details_database.pop("user_id",None)
+
+    db_order_products = get_order_products_database(order_id)
+
+    if not db_order_products or db_order_products == "DB_ERROR":
+        return False
+
+    order = {
+        "details":order_details_database,
+        "products":db_order_products
+    }
+    return order
+
+
