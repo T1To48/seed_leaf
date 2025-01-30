a33be0350d8d0dc11651c8498f5e80b75622fd65
T1to48 <tofek.98@gmail.com>
2025-01-30 16:59:36 +0000

    add styles global reset values

--- client/src/styles/global/Global.styles.ts
+++ client/src/styles/global/Global.styles.ts
@@ -1,13 +1,36 @@
 import { createGlobalStyle } from "styled-components";
+import theme from "./theme";
 
 const GlobalStyles = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap');
 
-body {
-font-family:"Open Sans", sans-serif;
+*,
+*::before,
+*::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
 
+html{
+    font-size:62.5%;
+}
 
+ body {
+    font-family:"Open Sans", sans-serif;
+    background:${props=>props.theme.color.background}
+}
+
+a {
+    text-decoration: none;
+    /* color: */
+}
+ul {
+    list-style: none;
+}
+img {
+    max-width: 100%;
+}
 `;
 
 export default GlobalStyles;
