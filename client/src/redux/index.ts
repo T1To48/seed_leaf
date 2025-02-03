5ee1fc2ae13c876c6b8c53bfd5962a176415a4a6
T1to48 <tofek.98@gmail.com>
2025-02-03 22:03:11 +0000

    add the slice reducer to the export index

--- client/src/redux/index.ts
+++ client/src/redux/index.ts
@@ -2,3 +2,8 @@ import { store } from "./store";
 import { useAppDispatch, useAppSelector } from "./hooks";
 
 export {store,useAppDispatch, useAppSelector}
+
+
+import { toggleMobileMode } from "./stateManagment/ui.slice";
+
+export {toggleMobileMode}
