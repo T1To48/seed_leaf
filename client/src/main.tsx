3b72c3fcb77d5975f0acf75ac1f2e3c707190724
T1to48 <tofek.98@gmail.com>
2025-02-03 20:03:51 +0000

    add the redux store provider to the main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import { GlobalStylesProvider } from "./providers"
+import { GlobalStylesProvider, ReduxStoreProvider } from "./providers"
 
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GlobalStylesProvider>
-      <App />
-    </GlobalStylesProvider>
+    <ReduxStoreProvider>
+      <GlobalStylesProvider>
+        <App />
+      </GlobalStylesProvider>
+    </ReduxStoreProvider>
   </StrictMode>,
 )
