4336039830a1f25c5df8a08ef28a3409f2b63c7d
T1to48 <tofek.98@gmail.com>
2025-02-03 11:33:07 +0000

    main app component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,20 @@
+import { Navbar } from "./components"
 function App() {
-
   return (
-    <div>
-    </div>
+    <>
+      <Navbar/>
+    </>
   )
 }
 
 export default App
 
+// useEffect(() => {
+//   window.addEventListener("resize",()=>{
+//     console.log(window.innerWidth)
+//   })
+
+//   return () => {
+//     window.removeEventListener("resize",()=>{})
+//   }
+// }, [])
