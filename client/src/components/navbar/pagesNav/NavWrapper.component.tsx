e1794662863eba6cd168f92fe8da51b7d5009e00
T1to48 <tofek.98@gmail.com>
2025-02-03 11:45:55 +0000

    pages nav wrapper (contain the onScroll toggle functinaly)

---
+++ client/src/components/navbar/pagesNav/NavWrapper.component.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState, useRef } from "react"
+import styled from "styled-components"
+import { FCWithChildren } from "../../../types"
+const StyledNav = styled.nav<{ isHidden: boolean }>`
+width:100%;
+  padding: 0.5rem 2rem;
+  position:fixed;
+  top:${({ isHidden }) => (isHidden ? "0px" : "70px")};
+  right:0;
+  left:0;
+  z-index:900;
+  background-color:${({ theme }) => theme.color.primary};
+  transition: top 0.4s ease-in-out;
+  `
+const NavWrapper: FCWithChildren = ({ children }) => {
+    const [isHidden, setIsHidden] = useState(false)
+    const lastScrollY = useRef(window.scrollY);
+
+    useEffect(() => {
+        const togglePagesNav = () => {
+            const currentScrollY = window.scrollY;
+            if (currentScrollY > lastScrollY.current + 10) {
+                setIsHidden(true)
+            }
+            else if (currentScrollY < lastScrollY.current - 10) {
+                setIsHidden(false)
+            }
+            lastScrollY.current = currentScrollY
+            console.log(lastScrollY)
+        }
+        window.addEventListener("scroll", togglePagesNav)
+        return () => window.removeEventListener("scroll", togglePagesNav)
+    }, [])
+
+    return (
+        <StyledNav isHidden={true}>
+            {children}
+        </StyledNav>
+    )
+}
+
+export default NavWrapper
