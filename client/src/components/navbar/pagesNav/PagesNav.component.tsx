d3b0ed264a96258fb5b30bac8c332016a7e92e34
T1to48 <tofek.98@gmail.com>
2025-02-03 11:44:52 +0000

    pages nav (toggles on scrolling)

---
+++ client/src/components/navbar/pagesNav/PagesNav.component.tsx
@@ -0,0 +1,65 @@
+import styled from "styled-components"
+import NavWrapper from "./NavWrapper.component"
+
+const StyledNavContainer = styled.div`
+  max-width:${({theme}) => theme.siteMaxWidth.desktop};
+  margin:0 auto;
+  display:flex;
+  justify-content:center;
+  align-items: center;
+  gap:7rem;
+  height: 40px;
+  color:${({ theme }) => theme.color.secondary};
+  font-size:${({ theme }) => theme.fontSize.normal.desktop} ;
+  font-weight:${({ theme }) => theme.fontWeight.semiBold};
+  letter-spacing: 1px;
+  
+`
+const StyledNavLink = styled.a`
+    position:relative;
+    color:${props => props.theme.color.secondary};
+    transition:0.2s ease-out;
+  
+  &::before{
+    content: "";
+    position: absolute;
+    background: ${props => props.theme.color.secondary};
+    bottom:-5px;
+    left:50%;
+    width:0;
+    height:2px;
+    transition:width 0.3s ease-out, left 0.3s ease-out;
+    transform: translateX(-50%);
+  }
+  &:hover::before{
+    width: 90%;
+  }
+
+`
+const PagesNav = () => {
+    return (
+        <NavWrapper>
+            <StyledNavContainer>
+
+                <div >
+                    <StyledNavLink href="https://google.com">HOME</StyledNavLink>
+                </div>
+
+                <div>
+                    <StyledNavLink href="https://google.com">SHOP</StyledNavLink>
+                </div>
+
+                <div>
+                    <StyledNavLink href="https://google.com">CONCEPT</StyledNavLink>
+                </div>
+
+                <div>
+                    <StyledNavLink href="https://google.com">GALLERY</StyledNavLink>
+                </div>
+
+            </StyledNavContainer>
+        </NavWrapper>
+    )
+}
+
+export default PagesNav
