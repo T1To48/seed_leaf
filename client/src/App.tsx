2a1dd5d0a7401b2bca01969a94799fb43d985de4
T1to48 <tofek.98@gmail.com>
2025-02-04 23:45:17 +0000

    react-router setup. initial, not final routes structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,22 @@
-import { Navbar } from "./components"
-import { useCheckDeviceType } from "./customHooks"
+import { useCheckDeviceType } from "./customHooks";
+import { BrowserRouter, Routes, Route } from "react-router";
 
+import { SharedLayouts } from "./layouts";
 function App() {
-  useCheckDeviceType()
-  
+  useCheckDeviceType();
+
   return (
-    <>
-      <Navbar/>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SharedLayouts />}>
+          <Route index element={<h1 style={{marginTop:"12rem"}}>HOME</h1>} />
+          <Route path="/products" element={<h1>STORE</h1>} />
+          <Route path="/concept" element={<h1>CONCEPT</h1>} />
+          <Route path="/gallery" element={<h1>GALLERY</h1>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
