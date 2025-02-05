307a72d60afb9ecda381c6d4cdd244de9da07dc1
T1to48 <tofek.98@gmail.com>
2025-02-05 00:30:50 +0000

    Products route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,7 +10,10 @@ function App() {
       <Routes>
         <Route path="/" element={<SharedLayouts />}>
           <Route index element={<h1 style={{marginTop:"12rem"}}>HOME</h1>} />
-          <Route path="/products" element={<h1>STORE</h1>} />
+          <Route path="products"  >
+            <Route index element={<h1 style={{marginTop:"12rem"}}>products</h1>} />
+            <Route path=":productId" element={<h1 style={{marginTop:"12rem"}}>product Page</h1>} />
+          </Route>
           <Route path="/concept" element={<h1>CONCEPT</h1>} />
           <Route path="/gallery" element={<h1>GALLERY</h1>} />
         </Route>
