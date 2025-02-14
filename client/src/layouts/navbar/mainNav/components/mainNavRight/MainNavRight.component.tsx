6573079ee5ba6f14972a21d17a9e081087719d92
T1to48 <tofek.98@gmail.com>
2025-02-14 23:52:46 +0000

    mainnav right side area

---
+++ client/src/layouts/navbar/mainNav/components/mainNavRight/MainNavRight.component.tsx
@@ -0,0 +1,13 @@
+import { StyledNavItem} from "../common.styles"
+import NavClientActions from "./NavClientActions.component"
+
+const MainNavRight = () => {
+    
+  return (
+    <StyledNavItem>
+        <NavClientActions/>
+    </StyledNavItem>
+  )
+}
+
+export default MainNavRight
