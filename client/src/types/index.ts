dd683f0c7a6d4aee9bea337d71e59bc830d2dac5
T1to48 <tofek.98@gmail.com>
2025-02-14 23:47:39 +0000

    add the new nav types to the types index export

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,2 +1,5 @@
 import type { FCWithChildren } from "./global.types";
 export type {FCWithChildren}
+
+import type {MainNavLeftProps, MainNavSearchBar} from "./navbar.types"
+export type {MainNavLeftProps, MainNavSearchBar}
