2c615a633873c3ef52467715ee60ad03a7d1b75d
T1to48 <tofek.98@gmail.com>
2025-02-14 23:51:27 +0000

    refactored navclientactions component

---
+++ client/src/layouts/navbar/mainNav/components/mainNavRight/NavClientActions.component.tsx
@@ -0,0 +1,29 @@
+import {
+  StyledActionsList,
+  StyledActionItem,
+  StyledProfileIcon,
+} from "./NavClientActions.styles";
+import { UserIcon, ShoppingCartIcon } from "@heroicons/react/24/outline";
+import {
+  XCircleIcon,
+  // CheckCircleIcon
+} from "@heroicons/react/16/solid";
+
+const NavClientActions = () => {
+  return (
+    <StyledActionsList>
+      <StyledActionItem>
+        <StyledProfileIcon>
+          <XCircleIcon />
+          <UserIcon />
+        </StyledProfileIcon>
+      </StyledActionItem>
+
+      <StyledActionItem>
+        <ShoppingCartIcon />
+      </StyledActionItem>
+    </StyledActionsList>
+  );
+};
+
+export default NavClientActions;
