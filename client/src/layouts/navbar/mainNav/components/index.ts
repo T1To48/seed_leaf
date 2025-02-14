f930ff348aa2fe3d456b612c4d595f9628cb4657
T1to48 <tofek.98@gmail.com>
2025-02-14 23:56:58 +0000

    main nav components export index

---
+++ client/src/layouts/navbar/mainNav/components/index.ts
@@ -0,0 +1,9 @@
+import MainNavLeft from "./mainNavLeft/MainNavLeft.component";
+import MainNavMiddle from "./mainNavMiddle/MainNavMiddle.component";
+import MainNavRight from "./mainNavRight/MainNavRight.component";
+
+import MainNavMobileSearch from "./mainNavMobileSearch/MainNavMobileSearch.component";
+
+import NavSearch from "./common/NavSearch.component";
+
+export { MainNavLeft, MainNavMiddle, MainNavRight, MainNavMobileSearch, NavSearch }
