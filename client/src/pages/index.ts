5a9f81fb91b458010f1febce14adb01c12b9c4de
T1to48 <tofek.98@gmail.com>
2025-03-03 14:47:25 +0000

    export indexes files

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,3 +1,5 @@
 import Home from "./home/Home.page";
+import Store from "./store/Store.page";
+import ProductPage from "./productPage/Product.page";
 
-export {Home}
+export {Home, Store,ProductPage}
