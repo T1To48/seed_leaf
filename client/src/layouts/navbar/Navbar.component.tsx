00629d2a86b91e92b65dbab658a42f4aff36a5c8
T1to48 <tofek.98@gmail.com>
2025-03-03 13:42:30 +0000

    add close hamburger menu function

--- client/src/layouts/navbar/Navbar.component.tsx
+++ client/src/layouts/navbar/Navbar.component.tsx
@@ -1,15 +1,18 @@
 import MainNav from "./mainNav/MainNav.component";
 import PagesNav from "./pagesNav/PagesNav.component";
 import HamburgerManu from "./hamburgerMenu/HamburgerMenu.component";
-import { useAppSelector } from "src/redux";
+import { toggleHamburgerMenu, useAppDispatch, useAppSelector } from "src/redux";
 
 const Navbar = () => {
+  const dispatch= useAppDispatch()
   const { isMobile, isHamburgerMenuOpen } = useAppSelector((state) => state.ui);
-  
+  const handleHamburgerMenuClose = ()=>{
+    dispatch(toggleHamburgerMenu(false))
+  }
   return (
     <div aria-label="nav">
       <MainNav />
-      {isMobile ? <HamburgerManu isOpen={isHamburgerMenuOpen} /> : <PagesNav />}
+      {isMobile ? <HamburgerManu handleClose={handleHamburgerMenuClose} isOpen={isHamburgerMenuOpen} /> : <PagesNav />}
     </div>
   );
 };
