479f619a6420d5bc79d9d79cef08669cca3c1db1
T1to48 <tofek.98@gmail.com>
2025-03-05 11:22:22 +0000

    add login and register pages to react-router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { useCheckDeviceType } from "./customHooks";
 import { BrowserRouter, Routes, Route } from "react-router";
 
 import { SharedLayouts } from "./layouts";
-import { Home, Store, ProductPage } from "./pages";
+import { Home, Store, ProductPage, Cart, Register, Login } from "./pages";
 function App() {
   useCheckDeviceType();
 
@@ -10,13 +10,16 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SharedLayouts />}>
-          <Route index element={<Home />} />
-          <Route path="/store/:category?"  >
-            <Route index element={<Store/>} />
-            <Route path="view-product/:productId" element={<ProductPage/>} />
-          </Route>
           <Route path="/concept" element={<h1>CONCEPT</h1>} />
           <Route path="/gallery" element={<h1>GALLERY</h1>} />
+          <Route index element={<Home />} />
+          <Route path="/store/:category?">
+            <Route index element={<Store />} />
+            <Route path="view-product/:productId" element={<ProductPage />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register/>} />
         </Route>
       </Routes>
     </BrowserRouter>
