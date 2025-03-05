eafc0702ffd0fcdb715c6cd8c2cbe8f5e33d556d
T1to48 <tofek.98@gmail.com>
2025-03-05 11:23:29 +0000

    added login and register pages to export index

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,5 +1,8 @@
 import Home from "./home/Home.page";
 import Store from "./store/Store.page";
 import ProductPage from "./productPage/Product.page";
+import Cart from "./cart/Cart.page";
+import Login from "./authPages/Login.page";
+import Register from "./authPages/Register.page";
 
-export {Home, Store,ProductPage}
+export { Home, Store, ProductPage, Cart, Register, Login }
