96bf816322d5da0e20a4ce8b9fa6127ba6a4e878
T1to48 <tofek.98@gmail.com>
2025-03-06 23:16:14 +0000

    added checkout to pages export index

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -2,7 +2,8 @@ import Home from "./home/Home.page";
 import Store from "./store/Store.page";
 import ProductPage from "./productPage/Product.page";
 import Cart from "./cart/Cart.page";
+import Checkout from "./checkout/Checkout.page";
 import Login from "./authPages/Login.page";
 import Register from "./authPages/Register.page";
 
-export { Home, Store, ProductPage, Cart, Register, Login }
+export { Home, Store, ProductPage, Cart, Checkout, Register, Login }
