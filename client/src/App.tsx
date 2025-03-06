e8f18091d50a4e23a2281ef9203d41b6d7ae7579
T1to48 <tofek.98@gmail.com>
2025-03-06 23:15:55 +0000

    added cart and checkout pages to react-router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,16 @@ import { useCheckDeviceType } from "./customHooks";
 import { BrowserRouter, Routes, Route } from "react-router";
 
 import { SharedLayouts } from "./layouts";
-import { Home, Store, ProductPage, Cart, Register, Login } from "./pages";
+import {
+  Home,
+  Store,
+  ProductPage,
+  Cart,
+  Checkout,
+  Register,
+  Login,
+} from "./pages";
+
 function App() {
   useCheckDeviceType();
 
@@ -17,9 +26,10 @@ function App() {
             <Route index element={<Store />} />
             <Route path="view-product/:productId" element={<ProductPage />} />
           </Route>
-          <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register/>} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
         </Route>
       </Routes>
     </BrowserRouter>
