955f049b872cd74fb2504e7dfa2736e8fc296a9b
T1to48 <tofek.98@gmail.com>
2025-05-06 00:00:01 +0300

    auth pages-  components  export index

---
+++ client/src/pages/authPages/components/index.ts
@@ -0,0 +1,8 @@
+import Form from "./form/Form.component";
+import SubmitBtn from "./buttonsAndLinks/SubmitBtn.component";
+import SeperationLine from "./seperationLine/SeperationLine.component";
+import RedirectLink from "./buttonsAndLinks/RedirectLink.compoent";
+import InputsList from "./input/InputsList.component";
+import { StyledNamesInputsContainer } from "./input/input.styles";
+
+export { Form, SubmitBtn, SeperationLine, RedirectLink, InputsList, StyledNamesInputsContainer }
