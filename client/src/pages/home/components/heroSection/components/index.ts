3f8621026450f191ffe00f3a4b94f106b6d73d5a
T1to48 <tofek.98@gmail.com>
2025-05-07 08:06:41 +0300

    exports file for hero components

---
+++ client/src/pages/home/components/heroSection/components/index.ts
@@ -0,0 +1,6 @@
+import BackgroundImg from "./backgroundImg/BackgroundImg.component";
+import Title from "./title/Title.component";
+import Text from "./text/Text.component";
+import Buttons from "./buttons/Buttons.component";
+
+export { BackgroundImg, Title, Text, Buttons }
