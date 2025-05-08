a1eace85550d2c7ac2698667f7bc29b95e3e6c4f
T1to48 <tofek.98@gmail.com>
2025-05-08 13:33:40 +0300

    exports for fact card components

---
+++ client/src/pages/home/components/factsSection/factCard/components/index.ts
@@ -0,0 +1,5 @@
+import FactLogo from "./FactLogo.component";
+import FactTitle from "./FactTitle.component";
+import FactText from "./FactText.component";
+
+export { FactLogo, FactTitle, FactText }
