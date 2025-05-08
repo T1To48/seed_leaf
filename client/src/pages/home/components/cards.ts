31bbda965ab4c286f7b690450dfbe7961001cd1e
T1to48 <tofek.98@gmail.com>
2025-05-08 13:31:46 +0300

    relocated, refactored components and files

--- client/src/pages/home/components/cards.ts
+++
@@ -1,9 +0,0 @@
-export const cards = [
-  { img: "https://supergruen.shop/cdn/shop/files/supergruen_Amarant-01_web_qua.jpg?v=1685456886", title: "Amaranth", price: "€5.50" },
-  { img: "https://supergruen.shop/cdn/shop/files/IMG_9833b_Erbsen_frei_web_qua.jpg?v=1685044864", title: "Peas", price: "€5.50" },
-  { img: "https://supergruen.shop/cdn/shop/files/supergruen_Salat-Mix-01_web_qua.jpg?v=1685545102", title: "SALTA", price: "€5.50" },
-  { img: "https://supergruen.shop/cdn/shop/files/supergrun_Brokkoli-02_web_qua.jpg?v=1685975548", title: "brokoli", price: "€5.50" },
-  { img: "https://supergruen.shop/cdn/shop/files/supergruen_Fenchel-01_web_qua.jpg?v=1685978995", title: "fenchel", price: "€5.50" },
-  { img: "https://supergruen.shop/cdn/shop/files/supergruen_gruenkohl-01_web_qua.jpg?v=1686039870", title: "vitamins", price: "€5.50" },
-  { img: "https://supergruen.shop/cdn/shop/files/Supergruen-Microgreens_Duo-Amarant-Rucola_frei_web.jpg?v=1713545321&width=1100", title: "radish", price: "€5.50" },
-];
