c5e5794a269101fd2358ad552ef8a36cfc1555ad
T1to48 <tofek.98@gmail.com>
2025-05-08 13:31:59 +0300

    home page sections exports file

---
+++ client/src/pages/home/components/index.tsx
@@ -0,0 +1,6 @@
+import HeroSection from "./heroSection/HeroSection.component";
+import FeaturedSection from "./featuredSection/FeaturedSection.component";
+import FactsSection from "./factsSection/FactsSection.component";
+import PurchaseStagesSection from "./purchaseStagesSection/PurchaseStagesSection.component";
+
+export { HeroSection, FeaturedSection, FactsSection, PurchaseStagesSection };
