907b37ace585a192b55acd1c7583c3877ad41a05
T1to48 <tofek.98@gmail.com>
2025-05-18 22:12:18 +0300

    mobile modal components export index

---
+++ client/src/pages/store/componentsRefactor/mobileStore/components/sortFilterModal/components/index.ts
@@ -0,0 +1,6 @@
+import ModalHeader from "./modalHeader/ModalHeader.component";
+import SortOptions from "./sortOptions/SortOptions.component";
+import FilterOptions from "./filterOptions/FilterOptions.component";
+import ConfirmBtn from "./confirmBtn/ConfirmBtn.component";
+
+export { ModalHeader, SortOptions, FilterOptions, ConfirmBtn }
