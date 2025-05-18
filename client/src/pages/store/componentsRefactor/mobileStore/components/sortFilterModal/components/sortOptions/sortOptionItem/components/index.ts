208a08f39844955af6035f9c08f28eeec1255447
T1to48 <tofek.98@gmail.com>
2025-05-18 22:11:26 +0300

    export file for sort modal components

---
+++ client/src/pages/store/componentsRefactor/mobileStore/components/sortFilterModal/components/sortOptions/sortOptionItem/components/index.ts
@@ -0,0 +1,5 @@
+import OptionInput from "./optionInput/OptionInput.component";
+import OptionLabel from "./optionLabel/OptionLabel.component";
+import OptionBtn from "./optionBtn/OptionBtn.component";
+
+export { OptionInput, OptionLabel, OptionBtn }
