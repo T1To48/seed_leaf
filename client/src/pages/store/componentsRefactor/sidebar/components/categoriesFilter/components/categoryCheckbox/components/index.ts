d3f258516b9a753ae00215d8e486230357e74f11
T1to48 <tofek.98@gmail.com>
2025-05-18 22:02:22 +0300

    checkbox components export file

---
+++ client/src/pages/store/componentsRefactor/sidebar/components/categoriesFilter/components/categoryCheckbox/components/index.ts
@@ -0,0 +1,5 @@
+import CheckboxInput from "./checkboxInput/CheckboxInput.component";
+import CheckboxLabel from "./checkboxLabel/CheckboxLabel.component";
+import CheckboxIcon from "./checkboxIcon/CheckboxIcon.component";
+
+export { CheckboxInput, CheckboxLabel, CheckboxIcon }
