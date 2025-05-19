e31762e7f9098180dab5ab269c1b929a6101dcd3
T1to48 <tofek.98@gmail.com>
2025-05-19 23:06:34 +0300

    checkoutPage , subcomponents export file

---
+++ client/src/pages/userPages/checkout/components/index.ts
@@ -0,0 +1,5 @@
+import CheckoutPageWrapper from "./checkoutPageWrapper/CheckoutPageWrapper.component";
+import PaymentSection from "./paymentSection/PaymentSection.component";
+import OrderOverview from "./orderOverview/OrderOverview.component";
+
+export { CheckoutPageWrapper, PaymentSection, OrderOverview }
