f67745d15ae11e6f5dc26ea81e8bac27300882da
T1to48 <tofek.98@gmail.com>
2025-05-19 23:04:24 +0300

    export file for left side section

---
+++ client/src/pages/userPages/checkout/components/paymentSection/components/index.ts
@@ -0,0 +1,4 @@
+import AddressNotice from "./addressNotice/AddressNotice.component";
+import PaymentDetails from "./paymentDetails/PaymentDetails.component";
+
+export { AddressNotice, PaymentDetails }
