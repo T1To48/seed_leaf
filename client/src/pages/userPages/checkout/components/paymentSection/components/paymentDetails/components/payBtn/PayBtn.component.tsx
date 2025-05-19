ec955c03d7588e78a1f84513c6913fa2c520be6c
T1to48 <tofek.98@gmail.com>
2025-05-19 23:00:18 +0300

    paying button

---
+++ client/src/pages/userPages/checkout/components/paymentSection/components/paymentDetails/components/payBtn/PayBtn.component.tsx
@@ -0,0 +1,10 @@
+import { StyledBtnWrapper, StyledPayBtn } from "./payBtn.styles";
+const PayBtn = () => {
+  return (
+    <StyledBtnWrapper>
+      <StyledPayBtn>Pay now</StyledPayBtn>
+    </StyledBtnWrapper>
+  );
+};
+
+export default PayBtn;
