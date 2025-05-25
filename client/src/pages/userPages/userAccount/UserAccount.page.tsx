d92463fbb0d2aeed06de36ed5b974fd4381d77cd
T1to48 <tofek.98@gmail.com>
2025-05-25 14:05:14 +0300

    added currentpage params hook

--- client/src/pages/userPages/userAccount/UserAccount.page.tsx
+++ client/src/pages/userPages/userAccount/UserAccount.page.tsx
@@ -1,8 +1,19 @@
+import { useNavigate, useParams } from "react-router";
+import type { RouteParams } from "src/types";
+import { linksTitles } from "src/constants";
 import { AccountWrapper, AccountSidebar, AccountHeader } from "./components";
+
 const UserAccount = () => {
+  const navigate = useNavigate();
+  const { currentPage } = useParams<{ currentPage: RouteParams }>();
+
+  if (!currentPage) {
+    navigate("/orders");
+    return null;
+  }
   return (
-    <AccountWrapper sidebar={<AccountSidebar />}>
-      <AccountHeader />
+    <AccountWrapper sidebar={<AccountSidebar currentPage={currentPage} />}>
+      <AccountHeader title={linksTitles[currentPage]} />
     </AccountWrapper>
   );
 };
