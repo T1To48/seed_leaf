73dab36f0642f4c43baedb7be78f15c4d14db63f
T1to48 <tofek.98@gmail.com>
2025-05-25 13:22:00 +0300

    sidebar username component-styles

---
+++ client/src/pages/userPages/userAccount/components/accountSidebar/components/sidebarUserName/sidebarUserName.styles.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const StyledUserName=styled.h2`
+  font-size: ${({theme})=>theme.fontSize.medium.desktop};
+  font-weight:${({theme})=>theme.fontWeight.semiBold};
+`
