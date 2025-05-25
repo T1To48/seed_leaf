a5366f54b596816dd6a1645d2d39a1e4ba4ba052
T1to48 <tofek.98@gmail.com>
2025-05-25 13:18:56 +0300

    navigation sidebar styles

---
+++ client/src/pages/userPages/userAccount/components/accountSidebar/accountSidebar.styles.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledSidebarWrapper = styled.aside`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 4rem;
+  width: 25%;
+  margin: 3rem 0 2rem;
+  padding: 1rem 0;
+  /* border-right: 0.12rem solid ${({ theme }) => theme.color.accent}; */
+  /* background-color: green; */
+`;
