0a03d84ebc2b599f0c05bfe4ef7ee2b20d7e401b
T1to48 <tofek.98@gmail.com>
2025-05-25 13:19:20 +0300

    sidebar components export file

---
+++ client/src/pages/userPages/userAccount/components/accountSidebar/components/index.ts
@@ -0,0 +1,4 @@
+import SidebarUserName from "./sidebarUserName/SidebarUserName.component";
+import SidebarLinks from "./sidebarLinks/SidebarLinks.component";
+
+export { SidebarUserName, SidebarLinks }
