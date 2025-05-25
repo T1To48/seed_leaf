31067cab3fbb2a04e14080f3fc65246a5f512cdf
T1to48 <tofek.98@gmail.com>
2025-05-25 13:17:39 +0300

    export file for account area components

---
+++ client/src/pages/userPages/userAccount/components/index.ts
@@ -0,0 +1,5 @@
+import AccountWrapper from "./accountWrapper/AccountWrapper.component";
+import AccountSidebar from "./accountSidebar/AccountSidebar.component";
+import AccountHeader from "./accountHeader/AccountHeader.component";
+
+export { AccountWrapper, AccountSidebar, AccountHeader }
