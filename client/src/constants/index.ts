89abb07541fd87217865de2e66f396f81e9c9dbd
T1to48 <tofek.98@gmail.com>
2025-05-25 14:04:18 +0300

    user account links constants

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,3 +1,7 @@
 import { defaultSortOption, otherSortOptionsArr } from "./store/store.constants";
 
 export { defaultSortOption, otherSortOptionsArr }
+
+
+import { linksArr, linksTitles } from "./userAccount/accountLinks";
+export { linksArr, linksTitles }
---
+++ client/src/constants/userAccount/accountLinks.tsx
@@ -0,0 +1,35 @@
+import type { SidebarLinkData } from "src/types";
+import {
+  ArrowRightStartOnRectangleIcon,
+  MapPinIcon,
+  ShoppingBagIcon,
+  UserIcon,
+} from "@heroicons/react/24/outline";
+
+export const linksArr: SidebarLinkData[] = [
+  {
+    text: "Orders History",
+    icon: <ShoppingBagIcon />,
+    routeParam: "orders",
+  },
+  {
+    text: "Manage Address",
+    icon: <MapPinIcon />,
+    routeParam: "address",
+  },
+  {
+    text: "Profile",
+    icon: <UserIcon />,
+    routeParam: "profile",
+  },
+  {
+    text: "Logout",
+    icon: <ArrowRightStartOnRectangleIcon />,
+  },
+];
+
+export const linksTitles = {
+  orders: "Orders History",
+  address: "Manage Address",
+  profile: "Profile",
+} as const;
