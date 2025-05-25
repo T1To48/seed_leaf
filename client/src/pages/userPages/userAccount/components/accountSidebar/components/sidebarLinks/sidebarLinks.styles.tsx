a6f9c14231f21e5094cc027b6dc7a2ff615ad9bd
T1to48 <tofek.98@gmail.com>
2025-05-25 13:20:53 +0300

    sidebar links wrapper (styles)

---
+++ client/src/pages/userPages/userAccount/components/accountSidebar/components/sidebarLinks/sidebarLinks.styles.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledLinksWrapper = styled.div`
+  width: 90%;
+  & > :last-child {
+    border-bottom: none;
+  & > :last-child{
+      visibility: hidden;
+    }
+  }
+`;
